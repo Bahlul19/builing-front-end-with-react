3d86db6025a79f9e556d19abe1612e5140195fb4
Bahlul19 <bahlul.tausif@gmail.com>
2021-12-28 12:58:20 +0600

    added the new userlist and user component

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import UserList from '../components/UserList';
 
 const Users = () => {
-    return <h1>User Works</h1>
+    const USERS = [{
+        id: 'u1',
+        name: 'Testing', 
+        image: 'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 
+        places:'Testing'
+    }];
+
+    return <UserList items = {USERS} />
 }
 export default Users;
