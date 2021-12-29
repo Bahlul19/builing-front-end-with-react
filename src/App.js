9fb361b87a3e276106540530f4539d23885baea1
Bahlul19 <bahlul.tausif@gmail.com>
2021-12-29 13:24:46 +0600

    Created the nav link section for the header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route,Redirect, Switch } from 'react-router-dom';
 import Users from './users/pages/Users';
 import NewPlaces from './places/pages/NewPlaces';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
-  return(
-
-  <Router>
-
-    <Switch>
-
-    <Route path="/" exact component={Users}>
-      <Users /> 
-    </Route>
-
-    <Route path="/places/new" exact component={NewPlaces}>
-      <NewPlaces /> 
-    </Route>
-
-    </Switch>
-
-    {/* <Redirect to="/" /> */}
-
-  </Router>
-
+  return (
+    <Router>
+      <main>
+      <MainNavigation />
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlaces />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
   );
 };
 
--- src/users/components/UserList.js
+++ src/users/components/UserList.js
@@ -1,13 +1,17 @@
 // import userEvent from '@testing-library/user-event';
 import React from 'react';
+
 import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
 import './UserList.css'
 
 const UserList = props => {
     if(props.items.length === 0){
         return(
             <div className='center'>
+                <Card>
                 <h2>User not found</h2>
+                </Card>
             </div>
         );
     }
