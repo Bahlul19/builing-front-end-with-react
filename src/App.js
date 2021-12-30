f1086cc6e56a9921e84ca2ff66e21eef10b0113e
Bahlul19 <bahlul.tausif@gmail.com>
2021-12-30 17:18:04 +0600

    Added the map on project with the view

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route,Redirect, Switch } from 'react-router-do
 import Users from './users/pages/Users';
 import NewPlaces from './places/pages/NewPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UserPlaces from './places/pages/UserPlaces';
 
 const App = () => {
   return (
@@ -10,13 +11,21 @@ const App = () => {
       <main>
       <MainNavigation />
         <Switch>
+          
           <Route path="/" exact>
             <Users />
           </Route>
+
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+
           <Route path="/places/new" exact>
             <NewPlaces />
           </Route>
+
           <Redirect to="/" />
+
         </Switch>
       </main>
     </Router>
