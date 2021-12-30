b6352f549af99d8893023cbbf1be264f1ae0dbc1
Bahlul19 <bahlul.tausif@gmail.com>
2021-12-30 12:20:29 +0600

    added the animation effect for menu on mobile device

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -9,40 +9,38 @@ import './MainNavigation.css';
 
 
 const MainNavigation = props => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
 
-    const openMyDrawer = () => {
-        setDrawerIsOpen(true);
-    }
-
-    const closeMyDrawer = () => {
-        setDrawerIsOpen(false);
-    }
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
 
   return (
-      <React.Fragment>
-          {drawerIsOpen && <Backdrop onClick={closeMyDrawer}/>}
-          {drawerIsOpen && (
-      <SideDrawer>
-          <nav className='main-navigation__drawer-nav'>
+    <React.Fragment>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
           <NavLinks />
-          </nav>
+        </nav>
       </SideDrawer>
-          )}
-    <MainHeader>
-      <button className="main-navigation__menu-btn" onClick={openMyDrawer}>
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">YourPlaces</Link>
-      </h1>
-      <nav className='main-navigation__header-nav'>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
     </React.Fragment>
   );
 };
